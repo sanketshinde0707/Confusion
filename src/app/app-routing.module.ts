d99debd8e3267071e1c3759f51893b04cf7b1cf8
sanketshinde0707 <56782192+sanketshinde0707@users.noreply.github.com>
2020-05-28 21:57:06 +0530

    Single Page Applications Part 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ import { AboutComponent } from './about/about.component';
 const routes: Routes = [
   {path: "home" , component: HomeComponent},
   {path: "menu" , component: MenuComponent},
+  {path: "dishdetail/:id" , component: DishdetailComponent},
   {path: "contactus" , component: ContactComponent},
   {path: "" , redirectTo: "/home" , pathMatch: "full"},
 ];
