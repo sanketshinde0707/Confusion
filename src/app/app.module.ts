4293141e2bc2b86c9759f66d18ea3fbf2bbca226
sanketshinde0707 <56782192+sanketshinde0707@users.noreply.github.com>
2020-06-16 16:31:20 +0530

    Reactive Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -57,7 +60,10 @@ import { LeaderService } from './services/leader.service';
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
+    MatSelectModule,
+    MatSlideToggleModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [
     DishService,
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Feedback, ContactType } from '../shared/feedback';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  feedbackForm: FormGroup; /*This is the form model which will host the reactive form */
+  feedback: Feedback; /*This is the dataa model , we an fetch data from data model thru thus*/
+  contactType = ContactType;
+
+  constructor(private fb: FormBuilder) {
+    this.createForm(); /* when this class is buit this form will be created*/
+   }
 
   ngOnInit(): void {
+  }
+
+
+  createForm() {
+    this.feedbackForm = this.fb.group({
+      firstname: '',
+      lastname: '',
+      telenum: 0,
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: '',
+    }) /*this is more like te feedback class, this doesnt has to be same , if it is
+        then it just becomes simple to map data from data model */
+  }
+
+  onSubmit() {
+    this.feedback = this.feedbackForm.value /* this gives a javascript object */
+    /* here the data model and the form model happens to be same so i can directly 
+    map the form model to data model . */
+    console.log(this.feedback);
+    this.feedbackForm.reset();
   }
 
 }
