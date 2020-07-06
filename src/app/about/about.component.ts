4cbefccf7dc912e3e8b832a2f156c9dcc9773f2b
sanketshinde0707 <56782192+sanketshinde0707@users.noreply.github.com>
2020-07-06 15:55:59 +0530

    Promise Part 1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -14,7 +14,7 @@ export class AboutComponent implements OnInit {
   leaders: Leader[];
 
   ngOnInit(): void {
-    this.leaders = this.leaderService.getLeaders();
+    this.leaderService.getLeaders().then(leaders => this.leaders = leaders);
   }
 
 }
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -12,8 +12,7 @@ import { DishService } from '../services/dish.service';
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
-
-   
+  
   dish: Dish;
 
   constructor(private dishService: DishService ,
@@ -23,7 +22,8 @@ export class DishdetailComponent implements OnInit {
 
   ngOnInit(): void {
     let id = this.route.snapshot.params['id'];
-    this.dish = this.dishService.getDish(id);
+    this.dishService.getDish(id)
+    .then( dish => this.dish = dish);
 
   }
 
